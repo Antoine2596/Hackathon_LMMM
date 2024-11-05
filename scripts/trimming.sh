0003dbba1e937a3baf5a97e303d0e74f561052d1
Antoine2596 <a.loth2000@gmail.com>
2024-11-05 18:27:39 +0100

    silence n correction

--- scripts/trimming.sh
+++ scripts/trimming.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+# j'ai ajouter la création à l'instant du sif, je fais faire une boucle pour faire la vérif
 
 mkdir -p trimming
+
+if test -f cutadapt_v1.11.sif; then
+  echo "File exists"
+else
+   echo "This file exist bro"
+   sudo singularity build featureCounts_v1.4.6-p3.sif def_files/install_featuresCount_v1.4.6-p3.def
+fi
+
+sudo singularity build cutadapt_v1.11.sif install_cutAdapt.def
 
 for file in mini_data/*gz ; do
         filename=$(basename "$file" ".fastq.gz")
