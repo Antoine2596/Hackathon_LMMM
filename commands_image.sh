d2d006f60e4a7047272b241786f0191d3dfcf715
Martin Amiens <128965195+MartinAmiens@users.noreply.github.com>
2024-11-07 19:11:34 +0100

    corrections
    corrections des sudo + ajouts des lignes manquantes + ajouts de commentaire

--- commands_image.sh
+++ commands_image.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-singularity build --fakeroot --force trimgalore.sif Singularity.def
-sudo singularity build bowtie_v0.12.7.sif def_files/install_bowtie.def
-sudo singularity build featureCounts_v1.4.6-p3.sif def_files/install_featuresCount_v1.4.6-p3.def
+# IL EST IMPORTANT DE SE PLACER DANS UN REPERTOIRE sif_files 
+# "mkdir sif_files "
+# "cd ./sif_files"
+
+# SRATOOLKIT
+# ajouter la version de SRATOOLKIT dans le nom de l'image (le .sif)
+singularity build --fakeroot SRATOOLKIT.sif ../def_files/install_SRATOOLKIT.def
+
+# BOWTIE
+singularity build --fakeroot bowtie_v0.12.7.sif ../def_files/install_bowtie.def
+
+# CUTADAPT
+singularity build --fakeroot cutAdapt.sif ../def_files/install_cutAdapt.def
+
+# FEATURECOUNTS
+singularity build --fakeroot featureCounts_v1.4.6-p3.sif ../def_files/install_featuresCount_v1.4.6-p3.def
