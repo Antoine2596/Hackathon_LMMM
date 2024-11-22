b57bbc14044ddfb85d7ccf0c96545a3ba22fac27
melodie <melodie.fleury@yahoo.fr>
2024-11-22 13:18:19 +0100

    *add main_deseq2 & 2 png images

---
+++ main_deseq2.r
@@ -0,0 +1,123 @@
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("EnrichmentBrowser")
+
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("DESeq2")
+
+#Chargement des librairies
+library("DESeq2")
+library("EnrichmentBrowser")
+
+#chemin absolu d'accès au répertoire du dossier des feature_counts --> A MODIFIER ! 
+path <- "C:/Users/Melo/Downloads/"
+
+#chemin absolu du dossier feature_counts --> NOM A MODIFIER !
+data_dir <- paste(path,"featureCounts_files_10M", sep = "")
+
+#Sélection des fichiers feature_counts dans une liste
+count_files <- list.files(
+  data_dir, 
+  pattern = "_counts.txt$", 
+  full.names = TRUE)
+
+#Fonction permettant de lire et charger les fichiers feature_counts
+read_count_file <- function(file) {
+  count_data <- read.table(
+    file, header = TRUE, 
+    stringsAsFactors = FALSE, 
+    comment.char = "#")
+  rownames(count_data) <- count_data$Geneid
+  count_data <- count_data[, ncol(count_data)]
+  return(count_data)
+}
+
+#Application de la fonction sur les fichiers
+all_counts <- do.call(cbind, lapply(count_files, read_count_file))
+#Remplacement des colonnes par le nom de chacun des échantillons
+colnames(all_counts) <- gsub("_counts.txt", "", basename(count_files))
+
+#Construction de la liste des conditions
+conditions <- factor(rep(
+  c(
+    "persister",
+    "control"),
+    each = 3
+    ))
+
+#Construction du dataframe associant chaque échantillon à sa condition (persister ou control)
+metadata <- data.frame(
+  sample = colnames(all_counts),
+  condition = conditions, 
+  row.names = colnames(all_counts)
+)
+
+## Check that match metadata and counts data
+all(colnames(all_counts) %in% rownames(metadata))
+all(colnames(all_counts) == rownames(metadata))
+
+## Create DESeq2Dataset object
+dds <- DESeqDataSetFromMatrix(countData = all_counts, 
+                              colData = metadata, 
+                              design = ~condition)
+
+View(counts(dds))
+
+#Perform median of ratios method of normalization
+dds <- DESeq(dds)
+res <- results(dds, alpha = 0.05) # alpha 0.05 par dépit
+
+#Affichage du résultat mean
+png(
+  filename = paste(
+    path,"Mean_of_normalized_counts_log_fold_change_.png"),
+    width = 1080, 
+    height = 1080)
+
+plotMA(
+  object = res,
+  colSig = "red",
+  colNonSig = "black"
+ )
+
+dev.off()
+
+#Normalization factor applied to each sample
+sizeFactors(dds)
+
+#Normalized counts matrix
+normalized_counts <- counts(dds, normalized=TRUE)
+
+View(normalized_counts)
+
+#Transformation en log
+logNormalizedCounts <- log2(normalized_counts + 1)
+
+png(
+  filename = paste(
+    path,"test_log_normalized_counts_log_fold_change.png"),
+    width = 1080, 
+    height = 1080)
+    
+#Affichage du résultat log
+limma::plotMA(logNormalizedCounts)
+abline(h=0)
+dev.off()
+
+#Download gene names + pathways
+browseVignettes("EnrichmentBrowser")
+kegg.gs <- getGenesets(org = "hsa", db = "kegg")
+length(kegg.gs)
+kegg.gs[1:2]
+
+sbea.res <- sbea(method = "ora", se = normalized_counts, gs = kegg.gs, perm = 0, alpha = 0.05) 
+gsRanking(sbea.res)
+
+aureus <- downloadPathways(
+    org = "hsa",
+    cache = TRUE,
+    out.dir = NULL,
+    zip = FALSE)
